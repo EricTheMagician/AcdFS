b6c82937e1ce5ed712ffb8094324295a22153768
Eric Yen <eric@ericyen.com>
2016-10-12 07:19:03 -0400

    Switch from priority queue to regular queue for cache. Use wait_for when waiting for a future to finish downloading. Clear DownloadCache while clearing PriorityCache

--- FileIO.cpp
+++ FileIO.cpp
@@ -2,6 +2,8 @@
 // Created by Eric Yen on 2016-10-06.
 //
 
+#define DOWNLOADINGNUMBERLOG 6
+
 #include "FileIO.h"
 #include "AcdApi.h"
 #include "easylogging++.h"
@@ -36,48 +38,53 @@ public:
     __no_collision_download__(){}
     ~__no_collision_download__(){}
     std::string buffer;
-    time_t last_access;
+    std::string cacheName;
     std::shared_future<std::string> future;
-    bool operator<(const  __no_collision_download__& rhs) const
-    {
-        return  last_access < rhs.last_access;
-    }
+//    bool operator<(const  __no_collision_download__& rhs) const
+//    {
+//        return  last_access < rhs.last_access;
+//    }
 };
 
 typedef std::shared_ptr< __no_collision_download__> DownloadItem;
 typedef std::weak_ptr< __no_collision_download__> WeakBuffer;
 
-
+/*
 class CacheCompare{
 public:
-bool operator()(const DownloadItem &b, const DownloadItem &a){
+bool operator()(const DownloadItem b, const DownloadItem a){
     if( a && b){
         if (a->buffer.length() > 0 && b->buffer.length() > 0) {
-            return a->last_access < b->last_access;
+            if(a.use_count() == b.use_count()) {
+                return a->last_access < b->last_access;
+            }else{
+                return a.use_count()  > b.use_count();
+            }
         }
 
         if (a->buffer.length() > 0) return true;
         if (b->buffer.length() > 0) return false;
         return a->last_access < b->last_access;
     }
-    if(a){
-        return false;
+    if(a.use_count() > 0){
+        return true;
     }
 
-    if(b){
-        return true;
+    if(b.use_count()> 0){
+        return false;
     }
 
     return false;
 
 }
 };
-
-static std::priority_queue<DownloadItem, std::vector<DownloadItem>, CacheCompare > PriorityCache{};
+*/
+//static std::priority_queue<DownloadItem, std::vector<DownloadItem>, CacheCompare > PriorityCache{};
+static std::queue<DownloadItem> PriorityCache{};
 static std::map<std::string, WeakBuffer> DownloadCache;
 
 inline uint64_t getChunkStart(uint64_t start){
-    int chunkNumber = start / BLOCK_DOWNLOAD_SIZE;
+    uint64_t chunkNumber = start / BLOCK_DOWNLOAD_SIZE;
     return chunkNumber * BLOCK_DOWNLOAD_SIZE;
 }
 
@@ -116,6 +123,8 @@ std::string FileIO::read(const size_t &size, const off_t &off) {
     return getFromCache(size, off );
 }
 
+static const int max_yield_count = 10000;
+
 std::string FileIO::getFromCache( const size_t &size, const off_t &off ){
 
     uint64_t chunkStart = getChunkStart(off);
@@ -137,18 +146,28 @@ std::string FileIO::getFromCache( const size_t &size, const off_t &off ){
         WeakBuffer cache = cursor->second;
         DownloadItem item = cache.lock();
         if(item) {
-            item->last_access = time(NULL);
             if(item->buffer.length() == 0){
                 int yield_count = 0;
-                const int max_yield_count = 10000000;
 
-                auto future = item->future;
-                while( (!future.valid()) && (item->buffer.length() == 0) ){
+                std::shared_future<std::string> future(item->future);
+
+                while( (!future.valid()) && (item->buffer.length() == 0) && (yield_count < max_yield_count) ){
 //                while( !(item->future.valid()) && (item->buffer.length() == 0) ){
                     std::this_thread::yield();
+                    yield_count++;
                 }
-                while( (item->buffer.length() == 0) ){
-                    item->future.wait();
+                if(yield_count == max_yield_count){
+                        // max yield reached;
+                    return getFromCache(size,off);
+                }
+
+
+                if( (item->buffer.length() == 0) ){
+                    VLOG(7) << "Waiting for 10 seconds!";
+                    std::future_status status = item->future.wait_for(std::chrono::seconds(10));
+                    if(status == std::future_status::timeout){
+                        return getFromCache(size, off);
+                    }
                 }
 
             }
@@ -156,7 +175,7 @@ std::string FileIO::getFromCache( const size_t &size, const off_t &off ){
             uint64_t start = off % BLOCK_DOWNLOAD_SIZE;
             uint64_t size2 = spillOver? BLOCK_DOWNLOAD_SIZE-off : size;
 //            LOG(TRACE) << "(off,size) = (" << start << ", " << size2  << ")";
-            item->last_access = time(NULL);
+//            item->last_access = time(NULL);
             buffer += item->buffer.substr(start, size2);
         }else{
             DownloadCache.erase(cacheName);
@@ -178,22 +197,33 @@ std::string FileIO::getFromCache( const size_t &size, const off_t &off ){
         ss << chunkStart2;
         cacheName = ss.str();
         cursor = DownloadCache.find(cacheName);
-        std::string buffer2;
+
         if (cursor != DownloadCache.cend()) {
             WeakBuffer cache = cursor->second;
             DownloadItem item = cache.lock();
             if(item) {
-                item->last_access = time(NULL);
                 if(item->buffer.length() == 0){
-                    auto future = item->future;
-                    while( (!future.valid()) && (item->buffer.length() == 0) ){
+                    std::shared_future<std::string> future(item->future);
+                    int yield_count = 0;
+                    while( (!future.valid()) && (item->buffer.length() == 0) && (yield_count < max_yield_count) ){
 //                    while( !(item->future.valid()) && (item->buffer.length() == 0) ){
                         std::this_thread::yield();
+                        yield_count++;
                     }
-                    while( (item->buffer.length() == 0) ){
-                        LOG(TRACE) << "Waiting!";
-                        item->future.wait();
+
+                    if(yield_count == max_yield_count){
+                        // max yield reached;
+                        return getFromCache(size,off);
                     }
+
+                    if( (item->buffer.length() == 0) ){
+                        std::future_status status = item->future.wait_for(std::chrono::seconds(10));
+                        if(status == std::future_status::timeout){
+                            return getFromCache(size, off);
+                        }
+                    }
+
+                    item.reset();
 
 //                    LOG(TRACE) << "Buffer length was either valid :\t" <<item->buffer.length() ;
                 }
@@ -202,6 +232,8 @@ std::string FileIO::getFromCache( const size_t &size, const off_t &off ){
                 uint64_t size2 = (off+size)%BLOCK_DOWNLOAD_SIZE;
 
                 buffer += item->buffer.substr(start, size2);
+//                item->last_access = time(NULL);
+
             }else{
                 DownloadCache.erase(cacheName);
                 chunksToDownload.push_back(chunkStart2);
@@ -219,16 +251,17 @@ std::string FileIO::getFromCache( const size_t &size, const off_t &off ){
         uint64_t start = chunkStart;
         start += spillOver ? 2*BLOCK_DOWNLOAD_SIZE : BLOCK_DOWNLOAD_SIZE;
         uint64_t temp = 0;
-        for(int i = 0; i < NUM_BLOCK_READ_AHEAD; i++){
+        for(uint64_t i = 0; i < NUM_BLOCK_READ_AHEAD; i++){
             temp = start + i*BLOCK_DOWNLOAD_SIZE;
-            if (temp >= fileSize){
+            if (temp > fileSize){
                 break;
             }
             chunksToDownload.push_back(temp);
         }
     }
 
-    for(uint64_t start: chunksToDownload){
+    for(uint64_t &start: chunksToDownload){
+
         ss.str(std::string());
         ss << m_file->m_id;
         ss << "\t";
@@ -237,7 +270,8 @@ std::string FileIO::getFromCache( const size_t &size, const off_t &off ){
         cursor = DownloadCache.find(cacheName);
         if (cursor == DownloadCache.cend()) {
             DownloadItem cache = std::make_shared<__no_collision_download__>();
-            cache->last_access = time(NULL);
+//            cache->last_access = time(NULL);
+            cache->cacheName = cacheName;
 
             DownloadCache.insert(std::make_pair(cacheName,cache));
             PriorityCache.push(cache);
@@ -255,14 +289,16 @@ std::string FileIO::getFromCache( const size_t &size, const off_t &off ){
 }
 
 std::string FileIO::download(AcdApi *api, DownloadItem cache, std::string cacheName, uint64_t start, uint64_t end){
+//    VLOG(DOWNLOADINGNUMBERLOG) << "Downloading " << cacheName;
     LOG(TRACE) << "Downloading " << cacheName;
     cache->buffer = api->Download(m_file,start,end);
 //    auto f = std::async(std::launch::async, &AcdApi::Download, api, m_file,start,end);
 //    cache->buffer = f.get();
     while( (PriorityCache.size()*BLOCK_DOWNLOAD_SIZE) > MAX_CACHE_SIZE){
-        auto top = PriorityCache.top();
+        auto top = PriorityCache.front();
         PriorityCache.pop();
 //        std::cout << "Delete use count " << top.use_count() <<std::endl;
+        DownloadCache.erase(top->cacheName);
         top.reset();
     }
     return cache->buffer;
